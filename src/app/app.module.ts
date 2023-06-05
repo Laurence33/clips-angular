3d1e320e645241aeee88d2e5497ca643929e3fb4
Laurence Cortez <laurencecortez.com>
2023-06-05 17:54:49 +0800

    module with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { VideoModule } from './video/video.module';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule,
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    VideoModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
