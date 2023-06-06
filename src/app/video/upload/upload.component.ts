ffe25ffa2c45703632733bccd72560d8f8c2e32d
Laurence Cortez <laurencecortez.com>
2023-06-06 19:50:44 +0800

    toggle forms

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UploadComponent {
   isDragOver = false;
-
+  isVideoReady = false;
+  file: File | null = null;
   storeFile(event: Event) {
     this.isDragOver = false;
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    console.log(this.file);
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+
+    this.isVideoReady = true;
   }
 }
