1013fa171500dc30041c871e724feef62b7d1a14
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-06-23 23:53:33 +0800

    moving videojs css and disable view encapsulation to solve css selector issue

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ClipComponent implements OnInit {
   id: string = '';
