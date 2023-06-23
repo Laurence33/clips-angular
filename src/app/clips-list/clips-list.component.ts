046147f94c72a25ae037951e472c64f9ab72d375
Laurence Cortez <laurencecortez.com>
2023-06-23 21:38:50 +0800

    listen for scroll to bottom

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.scss']
+  styleUrls: ['./clips-list.component.scss'],
 })
-export class ClipsListComponent {
+export class ClipsListComponent implements OnInit, OnDestroy {
+  constructor() {}
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement;
+    const { innerHeight } = window;
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+    console.log({ scrollTop, offsetHeight, innerHeight });
+    if (bottomOfWindow) {
+      console.log('bottom of the window');
+    }
+  };
 }
